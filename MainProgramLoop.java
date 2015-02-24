ed015be6985e5b0f3319b77b465e5641dbab6a92
David Snyder <dsnyder74@gmail.com>
2015-02-24 14:25:38 -0500

    Update MainProgramLoop with comments

--- MainProgramLoop.java
+++ MainProgramLoop.java
@@ -1,23 +1,35 @@
-/**
- * 
+/** File: MainProgramLoop.java
+ *  @author David Snyder
  */
+
 package dgs.StepCounter.v1;
 
-/**
- * @author David
- *
- */
+
 public class MainProgramLoop {
 
-	/**
-	 * @param args
+
+	/* The program displays a dialog allowing the user to start and pause
+	 * a pulsing thread.  User can change duration between pulses.
+	 * 
+	 *  Messages are sent to stdout documenting what the program is doing.
 	 */
+	
 
 	public static void main(String[] args) {
+		
         System.out.println("Starting Main Thread...");
+        
+        // Create new Pulse Controller object, and set the duration between
+        // pulses to 800 ms.
         PulseControl myPulseController = new PulseControl(800);
+        
+        // Create the main app window.  Pass the controller object so the main
+        // app window can controll the pulses.
         StepGUI mainWindow = new StepGUI( myPulseController );
+        
+        // Visible please.
         mainWindow.setVisible(true);
+        
         System.out.println("End of Main Thread...");
 	}
 
